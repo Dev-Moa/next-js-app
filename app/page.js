c49a106d6067ebbbba40f9c87b3e24ed6b040a75
unknown <codewithmoha@gmail.com>
2023-07-05 13:49:30 +0300

    counter

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
+"use client"
 import Image from 'next/image'
 import { useState } from 'react'
 export default function Home() {
-  const [count, setCounter] = useState('')
+  const [count, setCounter] = useState(0)
   const onClick = (e) => {
     e.preventDefault()
 
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
       <div>{count}</div>
       <button onClick={() => setCounter(count + 1)}>add</button>
     </main>
